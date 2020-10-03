e768afb3a4d9eebfa99699a94c617729c0afc346
jeeatek <rlawlxor90@gmail.com>
2020-10-03 19:13:18 +0900

    Style: make components
    make 4 components for todo-list

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+h1 {
+  text-align: center;
+  color: #2f3b52;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <input type="text" placeholder="write what to do" />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+input::placeholder {
+  text-align: center;
+}
+</style>
