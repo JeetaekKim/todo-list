d4d3c9d53682902f617a6b76650c1d6b3f5bc6b2
jeeatek <rlawlxor90@gmail.com>
2020-10-05 00:40:02 +0900

    Feat: TodoInput
    Type what to do and save it

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,13 +1,68 @@
 <template>
-  <input type="text" placeholder="write what to do" />
+  <div class="inputBox shadow">
+    <input
+      type="text"
+      v-model="newTodoItem"
+      placeholder="Type what you have to do"
+      v-on:keyup.enter="addTodo"
+    />
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      //   console.log(this.newTodoItem);
+      //   localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      if (this.newTodoItem !== "") {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(value, value);
+        this.clearInput();
+      }
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    },
+  },
+};
 </script>
 
-<style>
-input::placeholder {
-  text-align: center;
+<style scoped>
+input:focus {
+  outline: none;
+}
+
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0px 5px 5px 0px;
+}
+
+.addBtn {
+  color: white;
+  vertical-align: middle;
 }
 </style>
